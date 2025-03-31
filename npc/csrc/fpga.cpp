1628f49bc1536c33997e686a7316b81dd1a2bbea
hniii98 <752708928@qq.com>
2025-03-31 19:38:04 +0900

    Finish verilator example in file npc

--- npc/csrc/fpga.cpp
+++ npc/csrc/fpga.cpp
@@ -15,30 +15,16 @@ int main(int argc, char** argv){
   contextp->commandArgs(argc, argv);
   Vtop *top = new Vtop{contextp};
 
-  /* Trace wave */
-  Verilated::traceEverOn(true);
-  VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace(tfp, 99);
-  tfp->open("waveform.vcd");
-
   /* Bind pinsn to nvboard */
   nvboard_bind_all_pins(top);
   nvboard_init();
 
   while(!contextp->gotFinish()){
-	//int a = rand() & 1;
-	//int b = rand() & 1;
-	//top->a = a;
-	//top->b = b;
 	top->eval();
-	tfp->dump(contextp->time());
 	contextp->timeInc(1);
-//	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-//	assert(top->f == (a ^ b));
 	nvboard_update();
   } 
 	
-  tfp->close();
 
   nvboard_quit();
   delete top;
