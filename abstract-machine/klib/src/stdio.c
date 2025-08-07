545fb9836774f772c77b815868b5d79ad913085b
hniii98 <752608928@qq.com>
2025-08-07 18:00:40 +0900

    fix: fix a bug in stdio.c

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,96 +6,160 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+typedef struct{  
+  char *buf; 
+  size_t pos;
+  size_t size;
+} BufContext;
+
+typedef void (*EmitFunc) (char ch, void *context); // callback
+
+static void stream_emitter(char ch, void *context){
+  putch(ch); // only support stdout for now.
+}
+
+static void buf_emitter(char ch, void *context){
+  BufContext *c = (BufContext *)context;
+
+  if(c->pos < c->size-1) c->buf[c->pos++] = ch; // safely writing
+}
+
+// TODO: compose all emitter context to BufContext
+static void null_emitter(char ch, void *context){
+  /* Do nothing about emit, only countng */
+  (*(size_t *)context)++;
+}
+
+
+
+static int emit_number(EmitFunc emit, void *context, int num, int base){
+  char buf[64];
+  int len = 0;
+  if(num < 0){
+    emit('-', context);
+    num = -num;
+    len++;
+  }
+
+  do {
+    buf[len++] = "0123456789abcdef"[num % base];  
+  } while( num /= base );
+
+  for(int i = len-1; i >=0; i--) emit(buf[i], context);
+  return len;
+}
+
+static int emit_string(EmitFunc emit, void *context, const char* str){
+  int len = 0;
+  while(*str){
+    emit(*str++, context); 
+    len++;
+  }
+
+  return len;
+}
+
+static int format_core(EmitFunc emit, void *context, const char *fmt, va_list ap){
+  int total = 0;
+
+  while(*fmt){
+    /* Fast path */
+    if(*fmt != '%'){
+      emit(*fmt++, context); // directly emit a chararcter.
+      total++;
+      continue;
+    }
+
+    /* Slow path */
+    fmt++; // skip '%' for now
+
+    if (*fmt == '\0') { // boundary check
+      emit('%', context);
+      total++;
+      break;
+    }
+
+    char spec = *fmt++; // character after '%'
+    switch (spec) {
+      case '%': // "%%" should output '%'
+        emit('%', context);
+        total++;
+        break;
+
+      case 'd':
+        total += emit_number(emit, context, va_arg(ap, int), 10);
+        break;
+
+      case 's':
+        total += emit_string(emit, context, va_arg(ap, char*));
+        break;
+
+      default: // invalid format 
+        emit('%', context); // emit the skipped '%'
+        emit(spec, context); 
+        total += 2;
+    }
+  }
+
+  return total;
+}
 
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-static void int_to_str(int num, char *str);
-int sprintf(char *out, const char *fmt, ...) {
-  char *ptr = out;  
   va_list ap;
   va_start(ap, fmt);
 
-
-  char *s;
-  int d;
-  
-  while(*fmt != '\0'){
-
-    switch (*fmt){
-      case '%' : /* format character  */ 
-        fmt++; // we need the next flag
-
-        if(*fmt == 's'){ /* string */
-          s = NULL;
-          s = va_arg(ap, char *);
-          ptr = mempcpy(ptr, s, strlen(s));  
-        } 
-        else if(*fmt == 'd'){ /* int */
-          char buf[100] = {0}; // large enough to store int character 
-          d = va_arg(ap, int);
-          int_to_str(d, buf);
-          ptr = mempcpy(ptr, buf, strlen(buf));
-        }
-        else{
-          /* error occur, invalid flag, return a negative number*/
-          return -1;
-        }
-
-        break;
-
-      default : /* normal character */
-        *ptr++ = *fmt;  // copy directly
-        break;
-    }
-
-    fmt++; // process next character
-  }
- 
-
-  /* append terminated character*/
-  *ptr = '\0';
+  int ret = format_core(stream_emitter, NULL, fmt, ap);
 
   va_end(ap);
+  return ret;
+}
 
-  return strlen(out);
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  BufContext ctx = {.buf = out, .pos = 0, .size = (size_t)-1 }; // unsafe size
+
+  int ret = format_core(buf_emitter, &ctx, fmt, ap);
+  
+  out[ctx.pos] = '\0'; // unsafe termination
+
+  return ret;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int ret = vsprintf(out, fmt, ap);
+
+  va_end(ap);
+  return ret;
   
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // counting the needed length
+  va_list ap_copy;
+  va_copy(ap_copy, ap);
+  int needed = format_core(null_emitter, &(size_t){0} , fmt, ap_copy);
+  va_end(ap_copy);
+
+  BufContext ctx = {.buf = out, .pos = 0, .size = n}; // safe size
+  format_core(buf_emitter, &ctx, fmt, ap);
+  out[ctx.pos] = '\0'; // safe termination
+
+  return needed;
 }
 
 
-/* Transfer a int number to corresponding string including sign */
-static void int_to_str(int num, char *str){
-  int i = 0, sign = num;
-  if(num < 0) num = -num; /* get the absolute value*/
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
 
-  /* transfer number to char form LSB to MSB */
-  do{
-    str[i++] = num % 10 + '0';
-  }while((num/=10) > 0);
+  int ret = vsnprintf(out, n, fmt, ap);
 
-  if(sign < 0) str[i++] = '-';
-  str[i] = '\0';
-
-  /* reverse str */
-  for(int j = 0; j < i/2; j++){
-    char tmp = str[j];
-    str[j] = str[i-j-1];
-    str[i-j-1] = tmp;
-  }
+  va_end(ap);
+  return ret;
 }
 
 #endif
