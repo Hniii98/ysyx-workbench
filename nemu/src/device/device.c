3245f4e9649cfaa64a78786d4cfc2eb0c7f08602
hniii98 <752608928@qq.com>
2025-08-08 14:48:09 +0900

    fix: fix missing RTC update logic

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -16,8 +16,10 @@
 #include <common.h>
 #include <utils.h>
 #include <device/alarm.h>
+#include <device/mmio.h>
 #ifndef CONFIG_TARGET_AM
 #include <SDL2/SDL.h>
+
 #endif
 
 void init_map();
@@ -42,6 +44,12 @@ void device_update() {
   last = now;
 
   IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+  IFDEF(CONFIG_HAS_TIMER,
+    do { /* Little endian */
+      mmio_write(CONFIG_RTC_MMIO    , 4, (uint32_t)now); // write low 32 bits
+      mmio_write(CONFIG_RTC_MMIO + 4, 4, now >> 32); // write high 32 bits
+    } while(0)                        
+  );
 
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
